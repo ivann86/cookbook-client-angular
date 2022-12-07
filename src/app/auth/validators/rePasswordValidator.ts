1991be29a42956faee875a48db60fc8b1cfd1682
Ivan Ivanov <ivan.m.ivanov@icloud.com>
2022-12-07 12:29:33 +0200

    confirm password validator

--- src/app/auth/validators/rePasswordValidator.ts
+++ src/app/auth/validators/rePasswordValidator.ts
@@ -1,7 +1,12 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-export function rePasswordValidator(...controls: string[]): ValidatorFn {
-  return (controlGroup: AbstractControl): ValidationErrors => {
+export function rePasswordValidator(passwordControl: string, rePasswordControl: string): ValidatorFn {
+  return (controlGroup: AbstractControl): ValidationErrors | null => {
+    const password = controlGroup.get(passwordControl)?.value;
+    const rePassword = controlGroup.get(rePasswordControl)?.value;
+    if (password === rePassword) {
+      return null;
+    }
     return { groupControlsValues: true };
   };
 }
